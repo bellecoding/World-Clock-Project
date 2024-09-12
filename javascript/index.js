cc232b3300a4252178ef3f14f2c001fa6fa47a27
bellecoding <isabellasantos100@hotmail.com>
2024-09-12 13:17:44 +0200

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -34,6 +34,9 @@ TokyoTimeElement.innerHTML = TokyoTime.format("h:mm:ss [<small>]A[</small>]");
 
 function updateCity(event) {
     let cityTimezone = event.target.value;
+    if (cityTimezone === "current") {
+        cityTimezone = moment.tz.guess();
+    }
     let cityName = cityTimezone.replace("_","    ").split("/")[1];
     let cityTime = moment().tz(cityTimezone);
     let citiesElement = document.querySelector("#cities");
